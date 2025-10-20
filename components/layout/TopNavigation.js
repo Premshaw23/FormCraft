5fc8a2476ccaa30f9c5891af623d4bd894bf5ec3
Premshaw23 <shawprem217@gmail.com>
2025-10-20 17:24:33 +0530

    bug fix

--- components/layout/TopNavigation.js
+++ components/layout/TopNavigation.js
@@ -144,7 +144,7 @@ export default function TopNavigation({ onMenuClick }) {
           </button>
 
           {/* Logo */}
-          <Link href="/dashboard" className="flex items-center space-x-2 group">
+          <Link href="/" className="flex items-center space-x-2 group">
             <div className="w-8 h-8 bg-gradient-to-br from-purple-500 to-pink-500 rounded-lg flex items-center justify-center">
               <Sparkles className="w-5 h-5 text-white" />
             </div>
